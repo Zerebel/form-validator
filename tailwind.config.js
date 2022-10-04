71a24a3d619e7a0b446c233407c4f220336726ee
Zerebel <kofitruth11@gmail.com>
2022-10-04 13:35:01 +0000

    html-donee

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./index.html", "./Js/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heebo: ["Heebo", defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
